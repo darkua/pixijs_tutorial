e84e4c9dc912a0979c5d7eca151ccb7a18fb4572
darkua <sergioveiga@gmail.com>
2017-02-23 17:15:20 +0000

    -> pan and zooming : ok -> positioning elements in stage + drag and drop on shift click : ok -> selection, still needs to handle pan and zoom correctly -> still missing solving the stop propagation of event handlers

--- app.js
+++ app.js
@@ -25,14 +25,15 @@
     renderer.autoResize = true;
     renderer.resize(window.innerWidth, window.innerHeight);
 
-    
-
+    //selection on top of it
     $(document.body).append("<div style='z-index:1000;position:absolute; display:none; border: 1px dashed white' id='tpSelectBox'></div>");
+    //no right click menu on canvas
     $('body').on('contextmenu', '#stage', function(e){ return false; });
 
+    //zoom function
     function zoom(s,x,y){
-     
         s = s > 0 ? 1.05 : 0.95;
+        
         var worldPos = {x: (x - stage.x) / stage.scale.x, y: (y - stage.y)/stage.scale.y};
         var newScale = {x: stage.scale.x * s, y: stage.scale.y * s};
         var newScreenPos = {x: (worldPos.x ) * newScale.x + stage.x, y: (worldPos.y) * newScale.y + stage.y};
@@ -45,31 +46,51 @@
           stage.scale.y = newScale.y;
         }
     };
-
-    var lastPos = null
-    
-    //zoomm
+    //ZOOM_LISTENER
     addWheelListener(canvasDOM, function (e) {
         zoom(e.deltaY, e.offsetX, e.offsetY)
     });
+
     
-    //pan
-    renderer.plugins.interaction.on("mousedown",function(e) {
-      console.log('stage DOWN')
-      lastPos = {x:e.offsetX,y:e.offsetY};
-    }).on("mouseup",function(event) {
-      console.log('stage UP')
-      lastPos = null;
-    }).on("mousemove",function(e){
-      console.log('stage MOVE')
-      if(lastPos) {
-        stage.x += (e.offsetX-lastPos.x);
-        stage.y += (e.offsetY-lastPos.y);  
-        lastPos = {x:e.offsetX,y:e.offsetY};
+    //PAN && SELECTION LISTEN TO SAME EVENT SO CAN BE LOADED ON SAME TIME
+    isPanOn=false;
+    function togglePan(){
+      console.log("isPanOn?",!isPanOn);
+      if(isPanOn){
+        removePan();
+        selection.start()
+      }else {
+        addPan();
+        selection.stop();
       }
-    }).on("addChild",function(child){
-      console.log("addChild",child)
-    });
+    };
+
+    //pan
+    function addPan(){
+      var lastPos;
+      $(canvasDOM).on("mousedown",function(e) {
+        lastPos = {x:e.offsetX,y:e.offsetY};
+        $(canvasDOM).on("mousemove",function(e){
+          stage.x += (e.offsetX-lastPos.x);
+          stage.y += (e.offsetY-lastPos.y);  
+          lastPos = {x:e.offsetX,y:e.offsetY};
+        })
+      }).on("mouseup",function(e) {
+        $(canvasDOM).off("mousemove");
+        lastPos = {x:e.offsetX,y:e.offsetY};
+        console.log("stage",stage.position)
+      });
+      isPanOn=true;
+    }
+
+    function removePan(){
+      $(canvasDOM)
+      .off("mousedown")
+      .off("mouseup")
+      .off("mousemove")
+      isPanOn=false;
+    }
+    
 
 
     //load an image and run the `setup` function when it's done
@@ -86,17 +107,64 @@
         selectHeight,
         leftPos,
         topPos,
-        selected;
+        selected,
+        r,
+        Rwidth,
+        Rheight;
     
-    //SELECTION
+    
+    
     stage.displayList = new PIXI.DisplayList();
     var stageLayer = new DisplayGroup(0, true);
     var dragLayer = new DisplayGroup(1000, false);
 
-    // Seat
-    // Row
+    //DRAG FUNCTIONS
+    function onDragStart(event) {
+      event.stopPropagation()
+      console.log('element down',event)
+      this.data = event.data;
+      this.oldGroup = this.displayGroup;
+      this.displayGroup = dragLayer;
+      this.defaultCursor="move"    
+      
+      this.alpha=0.7;
+      this.dragPoint = event.data.getLocalPosition(this.parent);
+      this.dragPoint.x -= this.x;
+      this.dragPoint.y -= this.y;
+      this.on('mousemove', onDragMove)
+          .on('touchmove', onDragMove);
+    }
 
+    function onDragEnd(event) {
+      console.log('element up')
+      event.stopPropagation()
+      this.displayGroup = this.oldGroup;
+      this.alpha=1;
+      this.data = null;
+      this.off('mousemove');
+    }
 
+    function onDragMove(event) {
+      console.log('element move')
+      event.stopPropagation()
+      var newPosition = this.data.getLocalPosition(this.parent);
+      var x = newPosition.x - this.dragPoint.x;
+      var y = newPosition.y - this.dragPoint.y;
+      console.log('moving',x,y);
+      this.x = x;
+      this.y = y;
+    }
+    function subscribe(obj) {
+        obj.on('mousedown', onDragStart)
+            .on('touchstart', onDragStart)
+            .on('mouseup', onDragEnd)
+            .on('mouseupoutside', onDragEnd)
+            .on('touchend', onDragEnd)
+            .on('touchendoutside', onDragEnd);
+            
+    } 
+
+    //ADD_OBJECT
     function addObject(container=stage,image="cat.png",x=0,y=0){
       //Create the `cat` sprite, add it to the stage, and render it
       var obj = new Sprite(resources[image].texture);
@@ -106,54 +174,6 @@
       obj.buttonMode=true;
       obj.displayGroup=stageLayer;
 
-      function onDragStart(event) {
-        event.stopPropagation()
-        this.data = event.data;
-        this.oldGroup = this.displayGroup;
-        this.displayGroup = dragLayer;
-        this.defaultCursor="move"    
-        
-        this.alpha=0.7;
-        this.dragPoint = event.data.getLocalPosition(this.parent);
-        this.dragPoint.x -= this.x;
-        this.dragPoint.y -= this.y;
-        this.on('mousemove', onDragMove)
-            .on('touchmove', onDragMove);
-      }
-
-      function onDragEnd(event) {
-        event.stopPropagation()
-        this.displayGroup = this.oldGroup;
-        this.alpha=1;
-        this.data = null;
-        this.off('mousemove');
-      }
-
-      function onDragMove(event) {
-        event.stopPropagation()
-        var newPosition = this.data.getLocalPosition(this.parent);
-        var x = newPosition.x - this.dragPoint.x;
-        var y = newPosition.y - this.dragPoint.y;
-        //prevent off renderer
-        if( x > 0 && 
-            x <= renderer.width && 
-            y > 0 &&
-            y <= renderer.height
-          ){
-            console.log('moving',x,y);
-            this.x = x;
-            this.y = y;
-        } 
-      } 
-      function subscribe(obj) {
-          obj.on('mousedown', onDragStart)
-              .on('touchstart', onDragStart)
-              .on('mouseup', onDragEnd)
-              .on('mouseupoutside', onDragEnd)
-              .on('touchend', onDragEnd)
-              .on('touchendoutside', onDragEnd);
-              
-      }
       subscribe(obj); // add events
       container.addChild(obj);
       return obj
@@ -170,14 +190,14 @@
       addObject();
       addKeyboard();
       addSelection();
+      addPan();
       state = play;
       gameLoop();
-
     }
-
+    var selection;
     function addSelection(){
       //state machine to load and unload current mouse event!!!!
-      var selection = new machina.Fsm( {
+      selection = new machina.Fsm( {
         initialize: function( options ) {
           console.log("MOUSE MANAGER ON",options);  
         },
@@ -185,45 +205,74 @@
         initialState: "uninitialized",
         actions:{
           clearSelectBox: function(){
-            mouseDownX = null;
-            mouseDownY = null;
-            leftPos = null;
-            topPos = null;
-            selectWidth = null;
-            selectHeight = null;
-            $("#tpSelectBox").hide();
-            $("#tpSelectBox").css({"width":0, "height":0});
+            // mouseDownX = null;
+            // mouseDownY = null;
+            // leftPos = null;
+            // topPos = null;
+            // selectWidth = null;
+            // selectHeight = null;
+            // $("#tpSelectBox").hide();
+            // $("#tpSelectBox").css({"width":0, "height":0});
           },
           addSelectBox : function(ev){
-            mouseDownX = ev.data.global.x;
-            mouseDownY = ev.data.global.y;
-            canvasLeft = $(renderer.view).position().left;
-            canvasTop = $(renderer.view).position().top;
-            $("#tpSelectBox").css({left:mouseDownX+canvasLeft, top:mouseDownY+canvasTop}).show();
+            // ev.stopPropagation();
+            // console.log("select box",ev);
+            // mouseDownX = ev.data.global.x;
+            // mouseDownY = ev.data.global.y;
+            // canvasLeft = $(renderer.view).position().left;
+            // canvasTop = $(renderer.view).position().top;
+            // $("#tpSelectBox").css({left:mouseDownX+canvasLeft, top:mouseDownY+canvasTop}).show();
+            
+            var local = ev.data.getLocalPosition(stage);
+            console.log("local", local);
+            leftPos = local.x
+            topPos = local.y;
+            r = this.actions.drawHitArea({x:leftPos,y:topPos,width:0,height:0});
+            stage.addChild(r);
+            r.x = leftPos;
+            r.y = topPos;
+            console.log('pos',r.position)
             renderer.plugins.interaction.on('mousemove', this.actions.drawSelectBox);
           },
           drawSelectBox : function(ev){
-            var x = ev.data.global.x;
-            var y = ev.data.global.y;
-            selectWidth = Math.abs(x - mouseDownX);
-            selectHeight = Math.abs(y - mouseDownY);
-            var minX = Math.min(ev.data.global.x, mouseDownX);
-            var minY = Math.min(ev.data.global.y, mouseDownY);
-            leftPos = minX+canvasLeft;
-            topPos = minY+canvasTop;
-            var posCss = {
-                  "left":leftPos, 
-                  "top":topPos,
-                  "width":selectWidth,
-                   "height":selectHeight
-             };
-             $("#tpSelectBox").css(posCss);
+
+            // ev.stopPropagation();
+            // var x = ev.data.global.x;
+            // var y = ev.data.global.y;
+            // console.log("select",x,y);
+            // selectWidth = Math.abs(x - mouseDownX);
+            // selectHeight = Math.abs(y - mouseDownY);
+            // var minX = Math.min(ev.data.global.x, mouseDownX);
+            // var minY = Math.min(ev.data.global.y, mouseDownY);
+            // leftPos = minX+canvasLeft;
+            // topPos = minY+canvasTop;
+            // var posCss = {
+            //       "left":leftPos, 
+            //       "top":topPos,
+            //       "width":selectWidth,
+            //        "height":selectHeight
+            //  };
+            //  $("#tpSelectBox").css(posCss);
+            var local = ev.data.getLocalPosition(stage);
+            console.log("local_m", local);
+            Rwidth = local.x-leftPos;
+            Rheight = local.y-topPos;
+            
           },
           transitionToSelect : function(ev){
             renderer.plugins.interaction.off('mousemove', this.actions.drawSelectBox);
-            var rect = {x:leftPos,y:topPos,width:selectWidth,height:selectHeight};
+            // var rect = {x:leftPos,y:topPos,width:selectWidth,height:selectHeight};
+            // stage.removeChild(r);
+            var rect = {x:r.x,y:r.y,width:r.width,height:r.height};
             this.transition("selected",rect);
           },
+          drawHitArea:function(rect){
+              var rectangle = new Graphics();
+              rectangle.lineStyle(1, 0xFF3300, 1);
+              rectangle.drawRect(rect.x, rect.y, rect.width, rect.height);
+              return rectangle;
+          },
+
           handleSelection:function(rect){
 
             //helping functions
@@ -280,58 +329,54 @@
 
             function drawHitArea(rect){
               var rectangle = new Graphics();
-              rectangle.lineStyle(0, 0xFF3300, 0);
+              rectangle.lineStyle(1, 0xFF3300, 1);
               rectangle.drawRect(rect.x, rect.y, rect.width, rect.height);
               return rectangle;
             }
             function drawSelection(rect){
               var rectangle = new Graphics();
+              rectangle.beginFill(1,0.5);
               rectangle.lineStyle(1, 0xFF3300, 1);
+              rectangle.endFill();
               rectangle.drawRect(rect.x, rect.y, rect.width, rect.height);
               return rectangle;
             }
 
             var hitArea = drawHitArea(rect);
-            var children = stage.children;
-            for (var i = children.length - 1; i >= 0; i--) {
-              if (hitTestRectangle(children[i],hitArea)) {
-                selected.addChild(children[i])
-              }
-            }
+            console.log(hitArea)
+            stage.addChild(hitArea);
+            console.log(stage.children)
+            // var children = stage.children;
+            // for (var i = children.length - 1; i >= 0; i--) {
+            //   if (hitTestRectangle(children[i],hitArea)) {
+            //     selected.addChild(children[i])
+            //   }
+            // }
             //user selected something
-            if(selected.children.length){
-              var rectangle = drawSelection(selected.getBounds());
-              selected.addChild(rectangle)
-              stage.addChild(selected)  
-            }
+            // if(selected.children.length){
+            //   var rectangle = drawSelection(selected.getBounds());
+            //   selected.addChild(rectangle)
+            //   stage.addChild(selected)  
+            // }
           },handleUnselection:function(){
             console.log('unselecting')
             var children = selected.children;
             children.pop()
-            // for (var i = children.length - 1; i >= 0; i--) {
-            //   selected.removeChild(children[i])
-            //   stage.addChild(children[i]);
-            // }
+            for (var i = children.length - 1; i >= 0; i--) {
+              selected.removeChild(children[i])
+              stage.addChild(children[i]);
+            }
             this.transition("unselected");
-          },dragStart:function(event){
-              renderer.plugins.interaction.off('mousedown', this.actions.handleUnselection)
-              this.data = event.data;
-              this.alpha = 0.5;
-              this.dragging = true;
-          },dragEnd:function(event){
-              this.alpha = 1;
+          },
+          unsubscribe : function(){
+            selected
+              .off("mousemove")
+              .off("mouseup")
+              .off("mousemove");
 
-              this.dragging = false;
-
-              // set the interaction data to null
-              this.data = null;
-          },dragMove:function(event){
-              if (this.dragging)
-              {
-                  // var newPosition = this.data.getLocalPosition(this.parent);
-                  this.position.x = event.data.global.x;
-                  this.position.y = event.data.global.y;
-              }
+            renderer.plugins.interaction
+              .off("mousedown")
+              .off("mouseup")
           }
 
         },
@@ -339,7 +384,7 @@
           uninitialized: {
             "*": function() {
               this.deferUntilTransition();
-              this.transition( "unselected" );
+              this.transition("unselected");
             }
           },
           unselected: {
@@ -365,13 +410,13 @@
           selected: {
             _onEnter: function(area) {
               console.log("ENTER SELECTED",area);
+
               this.actions.handleSelection(area);
-              if(selected.children.length){
-                this.transition("movable");  
-              }else{
-                this.transition("unselected")
-              }
-              renderer.plugins.interaction.on('mouseup', this.actions.handleUnselection.bind(this));
+              // if(selected.children.length){
+              //   console.log(selected.)
+              // }else{
+                
+              // }
             },
             _onExit: function() {
               console.log("EXIT SELECTED");
@@ -379,25 +424,36 @@
           },
           movable:{
             _onEnter: function() {
+              console.log("ENTER MOVABLE");
               selected.defaultCursor="move"
-              selected
-                .on("mousedown",this.actions.dragStart.bind(this))
-                .on("mouseup",this.actions.dragEnd.bind(this))
-                .on("mousemove",this.actions.dragMove.bind(this))
+              subscribe(selected);
             },
             _onExit:function(){
-
+              console.log("EXIT MOVABLE");
+              selected
+              .off("mousemove")
+              .off("mouseup")
+              .off("mousemove");
+            }
+          },
+          off:{
+            _onEnter: function(){
+              console.log("ENTER OFF");
+              this.actions.clearSelectBox();
+              this.actions.unsubscribe();
+            },
+            _onExit : function(){
+              console.log("EXIT OFF");
             }
           }
         },
-        reset: function() {
-          this.handle( "_reset" );
-        },
         start : function(){
-            this.handle("unselected");
+          this.transition("unselected");
+        },
+        stop: function(){
+          this.transition("off");
         }
       });
-      // selection.start();
     };
 
 
@@ -411,32 +467,7 @@
     //   img.y = i;
     //   selected.addChild(img)
     // }
-    
-
-    function detectSelection(x,y,w,h){
-      
-      var area = drawSelectedArea(x,y,w,h);
-      var children = stage.children;
-      for (var i = children.length - 1; i >= 0; i--) {
-        if (hitTestRectangle(children[i],area)) {
-          selected.addChild(children[i])
-        }
-      }
-      area = null; // clear the selection
-      
-      //user selected something
-      if(selected.children.length){
-        var rectangle = drawSelection(selected.getBounds())
-        selected.addChild(rectangle)
-        stage.addChild(selected)  
-      }
-    }
-
-    
-
-    
-
-    
+  
 
     function keyboard(keyCode) {
       var key = {};
@@ -480,7 +511,9 @@
       var left = keyboard(37),
           up = keyboard(38),
           right = keyboard(39),
-          down = keyboard(40);
+          down = keyboard(40),
+          selection = keyboard(83),
+          shift = keyboard(16);
 
       //Left arrow key `press` method
       left.press = function() {
@@ -533,6 +566,19 @@
           selected.vy = 0;
         }
       };
+
+      selection.press = function(){
+        togglePan();
+      };
+
+      //Down
+      shift.press = function() {
+        removePan();
+      };
+      shift.release = function() {
+        addPan();
+      };
+
     }
 
     function gameLoop() {
@@ -545,7 +591,20 @@
         selected.x += selected.vx;
         selected.y += selected.vy  
       }
+      if(r){
+        r.width=Rwidth;
+        r.height =Rheight;
+        console.log('fodase')
+      }
     }
+
+    renderer.plugins.interaction.on('rightdown', function(ev) {
+        // console.log("global",ev.data.global.x,ev.data.global.y);
+        console.log("local point on stage",ev.data.getLocalPosition(stage));
+        var local = ev.data.getLocalPosition(stage);
+        addObject(stage,"cat.png",local.x,local.y);
+
+    });
       
       
 
@@ -554,6 +613,4 @@
 
       
 
-       renderer.plugins.interaction.on('rightdown', function(ev) {
-           addObject(stage,"cat.png",ev.data.global.x,ev.data.global.y);
-       });
+       
